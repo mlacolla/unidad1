5b161ef0fc770fc83ecd46c93351b4f58d94d8f2
mlacolla <138715626+mlacolla@users.noreply.github.com>
2025-02-11 18:13:51 -0300

    Agregando archivos al repo

---
+++ js/practica.js
@@ -0,0 +1,11 @@
+// Crea variables con los siguientes tipos de datos (Number, string y boolean)
+// Muestra los valores de las variables en consola
+
+
+let nombre ="Micaela"
+let numero = 15
+let bolean = true
+
+console.log(nombre)
+console.log(numero)
+console.log(bolean)
