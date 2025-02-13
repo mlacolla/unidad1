7ff64d0b5565f60da7abea71f320c1e07a01652f
mlacolla <138715626+mlacolla@users.noreply.github.com>
2025-02-13 13:36:58 -0300

    Practicas previas

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Crea variables con los siguientes tipos de datos (Number, string y boolean)
+// Muestra los valores de las variables en consola
+
+//inicializar variables.
+
+// let nombre ="Micaela"
+
+//declaracion
+let nombre;
+// asignacion
+nombre ='Micaela';
+nombre ='Soe';
+
+//inicializar variables.
+let numero = 15
+let boolean = true
+
+console.log(nombre)
+console.log(numero)
+console.log(boolean)
+//constante: const no se puede asignar nuevo valor
+const fecha= 1875;
+console.log(fecha);
+
+// fecha= 1991;
+// console.log(fecha);
---
+++ js/script.js
@@ -0,0 +1,4 @@
+console.log("Soy lo mas")
+
+const titulo = document.getElementById("titulo");
+titulo.textContent= "Texto modificado"
