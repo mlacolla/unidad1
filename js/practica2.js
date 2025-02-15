b4b0125d5bac4bacb8f98e086563ee0583a39c03
mlacolla <138715626+mlacolla@users.noreply.github.com>
2025-02-15 13:27:25 -0300

    sinModif

--- js/practica2.js
+++ js/practica2.js
@@ -46,6 +46,20 @@ for (let i = 0; i < repeticiones; i++) {
 
 // console.log(textoUConcatenado)
 
+//profe
+const salida = "ESC"
+let textoUsuario = prompt("Ingrese su texto: ");
+console.log(textoUsuario)
+let textoUConcatenado= "";
+textoUConcatenado += " "+textoUsuario + " ";
+
+while (textoUsuario != salida) {
+    textoUsuario = prompt("Ingrese su texto: ");
+    textoUConcatenado += " "+textoUsuario + " ";
+    console.log(textoUsuario)
+ }
+
+console.log(textoUConcatenado)
 // // 3) Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces
 // let numero = prompt("Ingrese un número: ");
 // for (i = 0; i < numero; i++) {
